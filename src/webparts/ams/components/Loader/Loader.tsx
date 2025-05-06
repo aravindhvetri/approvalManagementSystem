2344ce2f73ccc44a81b0c9dd030fb61a78fcca6d
LOGESWARAN001 <logeswaran0178@gmail.com>
2025-05-06 13:02:05 +0530

    Logi Changes (06/05/2025)(01/01)

--- src/webparts/ams/components/Loader/Loader.tsx
+++ src/webparts/ams/components/Loader/Loader.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import { ProgressSpinner } from 'primereact/progressspinner';
+import * as React from "react";
+import { ProgressSpinner } from "primereact/progressspinner";
 import "../Loader/Loader.css";
 
 export default function Loader() {
-    return (
-        <div className="loader-container">
-            <ProgressSpinner style={{width: '50px', height: '50px'}} 
-            strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
-        </div>
-    );
+  return (
+    <div className="loader-container">
+      {/* <ProgressSpinner style={{width: '50px', height: '50px'}} 
+            strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" /> */}
+
+      <span className="loader"></span>
+    </div>
+  );
 }
